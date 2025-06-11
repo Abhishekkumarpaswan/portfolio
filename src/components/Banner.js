919643c70283950d204f331e603b0c03201f8472
Abhishek Kumar Paswan <524110060@nitkkr.ac.in>
2025-06-12 01:25:50 +0530

    updated

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import headerImg from "../assets/img/deadpool.png";
+import headerImg from "../assets/img/disney_image.png";
 import { ArrowRightCircle } from 'react-bootstrap-icons';
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
